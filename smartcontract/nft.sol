76efd983cd7e73adbc562c8347f42c448d2e63eb
mihirmohan38 <52306226+mihirmohan38@users.noreply.github.com>
2021-07-27 20:32:40 +0800

    ADDED TOKEN COUNTER

--- smartcontract/nft.sol
+++ smartcontract/nft.sol
@@ -1,19 +1,21 @@
-// SPDX-License-Identifier: MIT
 pragma solidity 0.8.0;
  
 import "https://github.com/0xcert/ethereum-erc721/src/contracts/tokens/nf-token-metadata.sol";
 import "https://github.com/0xcert/ethereum-erc721/src/contracts/ownership/ownable.sol";
  
 contract newNFT is NFTokenMetadata, Ownable {
- 
+    
+  uint256 public tokenCounter;
   constructor() {
     nftName = "Fund NFT";
     nftSymbol = "FUND";
+    tokenCounter = 0 ; 
   }
  
-  function mint(address _to, uint256 _tokenId, string calldata _uri) external onlyOwner {
-    super._mint(_to, _tokenId);
-    super._setTokenUri(_tokenId, _uri);
+  function mint(address _to, string calldata _uri) external onlyOwner {
+    super._mint(_to, tokenCounter);
+    super._setTokenUri(tokenCounter, _uri);
+    tokenCounter = tokenCounter + 1 ; 
   }
  
 }
