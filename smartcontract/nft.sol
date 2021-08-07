0c82bfc835a4397c6b2cde01fd66e80d36f69348
mihirmohan38 <52306226+mihirmohan38@users.noreply.github.com>
2021-08-07 23:58:34 +0800

    Update nft.sol

--- smartcontract/nft.sol
+++ smartcontract/nft.sol
@@ -1,4 +1,3 @@
-pragma solidity 0.8.0;
  
 import "https://github.com/0xcert/ethereum-erc721/src/contracts/tokens/nf-token-metadata.sol";
 import "https://github.com/0xcert/ethereum-erc721/src/contracts/ownership/ownable.sol";
@@ -12,8 +11,8 @@ contract newNFT is NFTokenMetadata, Ownable {
     tokenCounter = 0 ; 
   }
  
-  function mint(address _to, string calldata _uri) external onlyOwner {
-    super._mint(_to, tokenCounter);
+  function mint(string calldata _uri) external onlyOwner {
+    super._mint(msg.sender, tokenCounter);
     super._setTokenUri(tokenCounter, _uri);
     tokenCounter = tokenCounter + 1 ; 
   }
